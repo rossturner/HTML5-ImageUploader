95d5eef501a2f9ec6612cf88f959d5dc4adf8e4e
Ross Turner <ross.turner001@googlemail.com>
2012-01-21 16:49:48 +0000

    Added JsLint

--- src/main/webapp/js/ImageUploader.js
+++ src/main/webapp/js/ImageUploader.js
@@ -1,18 +1,19 @@
+var console = console || { log: function() {} };
+
 var ImageUploader = (function() {
-	var publicApi = {};	
-	var config = {
+	var publicApi = {}, config = {
 		debug: true
 	};
 	
 	publicApi.init = function(inputElement) {
-		if ((!inputElement.getAttribute) || inputElement.getAttribute('type') != 'file') {
+		if ((!inputElement.getAttribute) || inputElement.getAttribute('type') !== 'file') {
 			throw new Error('First argument to ImageUploader.init() must be an element of type="file"');
 		}
 		
 		if (config.debug) {
 			console.log('Initialised ImageUploader for '+inputElement);
 		}
-	}
+	};
 		
 	return publicApi;
 }());
