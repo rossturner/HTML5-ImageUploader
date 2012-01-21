eb62f54aca5783d2b7d79ba3656ec7e5f5e6a63a
Ross Turner <ross.turner001@googlemail.com>
2012-01-21 17:12:09 +0000

    Groundwork for handling file list from input element

--- src/main/webapp/js/ImageUploader.js
+++ src/main/webapp/js/ImageUploader.js
@@ -1,7 +1,8 @@
 var console = console || { log: function() {} };
 
 var ImageUploader = (function() {
-	var publicApi = {}, config = {
+	var publicApi = {};
+	var config = {
 		debug: true
 	};
 	
@@ -9,6 +10,14 @@ var ImageUploader = (function() {
 		if ((!inputElement.getAttribute) || inputElement.getAttribute('type') !== 'file') {
 			throw new Error('First argument to ImageUploader.init() must be an element of type="file"');
 		}
+		
+		inputElement.addEventListener('change', function(event) {
+			var cursor = 0;
+			console.log('inputElement.change() called, do something with inputElement.files');
+			for (; cursor < inputElement.files.length; ++cursor) {
+				console.log(inputElement.files[cursor].name);
+			}
+		}, false);
 		
 		if (config.debug) {
 			console.log('Initialised ImageUploader for '+inputElement);
