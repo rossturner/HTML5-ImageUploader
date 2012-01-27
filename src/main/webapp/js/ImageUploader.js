b670e6b45aaf2e237212c82b1b8e54d96e423909
ross-tu <ross.turner@tradermedia.co.uk>
2012-01-27 13:23:34 +0000

    Rewrote file list handling to use an array and completion callbacks so that multiple images do not block the page (a lot of images still causes it to fall over and make a mess though)

--- src/main/webapp/js/ImageUploader.js
+++ src/main/webapp/js/ImageUploader.js
@@ -18,93 +18,129 @@ var ImageUploader = (function() {
         privateApi.setConfig(customConfig);
 
         inputElement.addEventListener('change', function(event) {
-            var cursor = 0;
-            for (; cursor < inputElement.files.length; ++cursor) {
-                privateApi.handleFileSelection(inputElement.files[cursor]);
+            var fileArray = new Array();
+            for (var cursor = 0; cursor < inputElement.files.length; ++cursor) {
+                fileArray.push(inputElement.files[cursor]);
             }
+            privateApi.handleFileList(fileArray);
         }, false);
 
         if (config.debug) {
             console.log('Initialised ImageUploader for ' + inputElement);
         }
     };
+    
+    privateApi.handleFileList = function(fileArray) {
+        if (fileArray.length > 1) {
+            var file = fileArray.shift();
+            privateApi.handleFileSelection(file, function() {
+                privateApi.handleFileList(fileArray);
+            });
+        } else if (fileArray.length === 1) {
+            privateApi.handleFileSelection(fileArray[0], function() { console.log('completed!'); });
+        }
+    };
 
-    privateApi.handleFileSelection = function(file) {
-        console.log(file.name);
+    privateApi.handleFileSelection = function(file, completionCallback) {
+        if (config.debug) {
+            console.log(file.name + ' started at '+new Date().getTime());
+        }
 
         var img = document.createElement('img');
-        config.workspace.appendChild(img);
+        //config.workspace.appendChild(img);
         config.workspace.appendChild(document.createElement('br'));
         var reader = new FileReader();
         reader.onload = function(e) {
             img.src = e.target.result;
 
             setTimeout(function() {
-                var canvas = document.createElement('canvas');
+                privateApi.scaleImage(img, completionCallback);
+            }, 1);
 
-                canvas.width = img.width / 2;
-                canvas.height = img.height / 2;
-
-                config.workspace.appendChild(canvas);
-
-                //thumbnailer(canvas, img, 1024, 1);
-
-                // TODO draw straight from canvas to canvas with context.drawImage first
-                // argument as a canvas (should work!)
-
-                var context = canvas.getContext('2d');
-                context.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-                var resizedImage = document.createElement('img');
-                config.workspace.appendChild(resizedImage);
-
-                resizedImage.src = canvas.toDataURL('image/jpeg');
-
-                // ugh
-                setTimeout(function() {
-                    var canvas2 = document.createElement('canvas');
-
-                    canvas2.width = resizedImage.width / 2;
-                    canvas2.height = resizedImage.height / 2;
-
-                    config.workspace.appendChild(canvas2);
-                    canvas2.getContext('2d').drawImage(resizedImage, 0, 0, canvas2.width, canvas2.height);
-
-                    var quarterImage = document.createElement('img');
-                    config.workspace.appendChild(quarterImage);
-
-                    quarterImage.src = canvas2.toDataURL('image/jpeg');
-
-                    setTimeout(function() {
-                        var canvas3 = document.createElement('canvas');
-
-                        var scale = 1024 / canvas2.width;
-
-                        canvas3.width = canvas2.width * scale;
-                        canvas3.height = canvas2.height * scale;
-                        config.workspace.appendChild(canvas3);
-
-                        var srcImg = canvas2.getContext('2d').getImageData(0, 0, canvas2.width, canvas2.height);
-                        var destImg = canvas3.getContext('2d').createImageData(canvas3.width, canvas3.height);
-
-                        BilinearInterpolation(srcImg, destImg, scale);
-
-                        canvas3.getContext('2d').putImageData(destImg, 0, 0);
-
-                        //canvas3.getContext('2d').drawImage(quarterImage, 0, 0, canvas3.width, canvas3.height);
-
-                        var sizedImage = document.createElement('img');
-                        config.workspace.appendChild(sizedImage);
-
-                        sizedImage.src = canvas3.toDataURL('image/jpeg');
-
-                    }, 300);
-
-                }, 300);
-
-            }, 400);
         }
         reader.readAsDataURL(file);
+
+    };
+    
+    privateApi.getHalfScaleCanvas = function(canvas) {
+        var halfCanvas = document.createElement('canvas');
+        halfCanvas.width = canvas.width / 2;
+        halfCanvas.height = canvas.height / 2;
+        //config.workspace.appendChild(halfCanvas);
+        
+        halfCanvas.getContext('2d').drawImage(canvas, 0, 0, halfCanvas.width, halfCanvas.height);
+
+        //config.workspace.removeChild(canvas);
+        return halfCanvas;
+    }
+
+    privateApi.scaleImage = function(img, completionCallback) {
+
+        var canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height);
+        //config.workspace.appendChild(canvas);
+        
+        while (canvas.width >= (2 * 1024)) {
+            canvas = privateApi.getHalfScaleCanvas(canvas);
+        }
+
+        //config.workspace.appendChild(canvas);
+
+        var resizedImage = document.createElement('img');
+        config.workspace.appendChild(resizedImage);
+
+        setTimeout(function() {
+            resizedImage.src = canvas.toDataURL('image/jpeg');
+            //config.workspace.removeChild(canvas);
+
+            if (config.debug) {
+                console.log('Finished at '+new Date().getTime());
+                completionCallback();
+            }
+        }, 1);
+        // ugh
+//        setTimeout(function() {
+//            var canvas2 = document.createElement('canvas');
+//
+//            canvas2.width = resizedImage.width / 2;
+//            canvas2.height = resizedImage.height / 2;
+//
+//            config.workspace.appendChild(canvas2);
+//            canvas2.getContext('2d').drawImage(resizedImage, 0, 0, canvas2.width, canvas2.height);
+//
+//            var quarterImage = document.createElement('img');
+//            config.workspace.appendChild(quarterImage);
+//
+//            quarterImage.src = canvas2.toDataURL('image/jpeg');
+//
+//            setTimeout(function() {
+//                var canvas3 = document.createElement('canvas');
+//
+//                var scale = 1024 / canvas2.width;
+//
+//                canvas3.width = canvas2.width * scale;
+//                canvas3.height = canvas2.height * scale;
+//                config.workspace.appendChild(canvas3);
+//
+//                var srcImgData = canvas2.getContext('2d').getImageData(0, 0, canvas2.width, canvas2.height);
+//                var destImgData = canvas3.getContext('2d').createImageData(canvas3.width, canvas3.height);
+//
+//                BilinearInterpolation(srcImgData, destImgData, scale);
+//
+//                canvas3.getContext('2d').putImageData(destImgData, 0, 0);
+//
+//                //canvas3.getContext('2d').drawImage(quarterImage, 0, 0, canvas3.width, canvas3.height);
+//
+//                var sizedImage = document.createElement('img');
+//                config.workspace.appendChild(sizedImage);
+//
+//                sizedImage.src = canvas3.toDataURL('image/jpeg');
+//
+//            }, 300);
+//
+//        }, 300);
 
     };
 
--- src/main/webapp/js/philou.ch.bilinear.interpolation.js
+++ src/main/webapp/js/philou.ch.bilinear.interpolation.js
@@ -1,97 +1,50 @@
-// Taken from http://www.philou.ch/js-bilinear-interpolation.html
+// Modified from http://www.philou.ch/js-bilinear-interpolation.html
 // Credit to Philippe Strauss
 
-var BilinearInterpolation = (function() {
-
-    function ivect(ix, iy, w) {
-        // byte array, r,g,b,a
-        return ((ix + w * iy) * 4);
+var BilinearInterpolation = function(srcCanvasData, destCanvasData, scale) {
+    // c.f.: wikipedia english article on bilinear interpolation
+    // taking the unit square, the inner loop looks like this
+    function inner(f00, f10, f01, f11, x, y) {
+        var un_x = 1.0 - x;
+        var un_y = 1.0 - y;
+        return (f00 * un_x * un_y + f10 * x * un_y + f01 * un_x * y + f11 * x * y);
     }
-
-    function bilinear(srcImg, destImg, scale) {
-        // c.f.: wikipedia english article on bilinear interpolation
-        // taking the unit square, the inner loop looks like this
-        // note: there's a function call inside the double loop to this one
-        // maybe a performance killer, optimize this whole code as you need
-        function inner(f00, f10, f01, f11, x, y) {
-            var un_x = 1.0 - x;
-            var un_y = 1.0 - y;
-            return (f00 * un_x * un_y + f10 * x * un_y + f01 * un_x * y + f11 * x * y);
-        }
-        var i, j;
-        var iyv, iy0, iy1, ixv, ix0, ix1;
-        var idxD, idxS00, idxS10, idxS01, idxS11;
-        var dx, dy;
-        var r, g, b, a;
-        for (i = 0; i < destImg.height; ++i) {
-            iyv = i / scale;
-            iy0 = Math.floor(iyv);
+    var i, j;
+    var iyv, iy0, iy1, ixv, ix0, ix1;
+    var idxD, idxS00, idxS10, idxS01, idxS11;
+    var dx, dy;
+    var r, g, b, a;
+    for (i = 0; i < destCanvasData.height; ++i) {
+        iyv = i / scale;
+        iy0 = Math.floor(iyv);
+        // Math.ceil can go over bounds
+        iy1 = (Math.ceil(iyv) > (srcCanvasData.height - 1) ? (srcCanvasData.height - 1) : Math.ceil(iyv));
+        for (j = 0; j < destCanvasData.width; ++j) {
+            ixv = j / scale;
+            ix0 = Math.floor(ixv);
             // Math.ceil can go over bounds
-            iy1 = (Math.ceil(iyv) > (srcImg.height - 1) ? (srcImg.height - 1) : Math.ceil(iyv));
-            for (j = 0; j < destImg.width; ++j) {
-                ixv = j / scale;
-                ix0 = Math.floor(ixv);
-                // Math.ceil can go over bounds
-                ix1 = (Math.ceil(ixv) > (srcImg.width - 1) ? (srcImg.width - 1) : Math.ceil(ixv));
-                idxD = ivect(j, i, destImg.width);
-                // matrix to vector indices
-                idxS00 = ivect(ix0, iy0, srcImg.width);
-                idxS10 = ivect(ix1, iy0, srcImg.width);
-                idxS01 = ivect(ix0, iy1, srcImg.width);
-                idxS11 = ivect(ix1, iy1, srcImg.width);
-                // overall coordinates to unit square
-                dx = ixv - ix0;
-                dy = iyv - iy0;
-                // I let the r, g, b, a on purpose for debugging
-                r = inner(srcImg.data[idxS00], srcImg.data[idxS10], srcImg.data[idxS01], srcImg.data[idxS11], dx, dy);
-                destImg.data[idxD] = r;
+            ix1 = (Math.ceil(ixv) > (srcCanvasData.width - 1) ? (srcCanvasData.width - 1) : Math.ceil(ixv));
+            idxD = (j + destCanvasData.width * i) * 4;
+            // matrix to vector indices
+            idxS00 = (ix0 + srcCanvasData.width * iy0) * 4;
+            idxS10 = (ix1 + srcCanvasData.width * iy0) * 4;
+            idxS01 = (ix0 + srcCanvasData.width * iy1) * 4;
+            idxS11 = (ix1 + srcCanvasData.width * iy1) * 4;
+            // overall coordinates to unit square
+            dx = ixv - ix0;
+            dy = iyv - iy0;
+            // I let the r, g, b, a on purpose for debugging
+            r = inner(srcCanvasData.data[idxS00], srcCanvasData.data[idxS10], srcCanvasData.data[idxS01], srcCanvasData.data[idxS11], dx, dy);
+            destCanvasData.data[idxD] = r;
 
-                g = inner(srcImg.data[idxS00 + 1], srcImg.data[idxS10 + 1], srcImg.data[idxS01 + 1], srcImg.data[idxS11 + 1], dx, dy);
-                destImg.data[idxD + 1] = g;
+            g = inner(srcCanvasData.data[idxS00 + 1], srcCanvasData.data[idxS10 + 1], srcCanvasData.data[idxS01 + 1], srcCanvasData.data[idxS11 + 1], dx, dy);
+            destCanvasData.data[idxD + 1] = g;
 
-                b = inner(srcImg.data[idxS00 + 2], srcImg.data[idxS10 + 2], srcImg.data[idxS01 + 2], srcImg.data[idxS11 + 2], dx, dy);
-                destImg.data[idxD + 2] = b;
+            b = inner(srcCanvasData.data[idxS00 + 2], srcCanvasData.data[idxS10 + 2], srcCanvasData.data[idxS01 + 2], srcCanvasData.data[idxS11 + 2], dx, dy);
+            destCanvasData.data[idxD + 2] = b;
 
-                a = inner(srcImg.data[idxS00 + 3], srcImg.data[idxS10 + 3], srcImg.data[idxS01 + 3], srcImg.data[idxS11 + 3], dx, dy);
-                destImg.data[idxD + 3] = a;
-            }
+            a = inner(srcCanvasData.data[idxS00 + 3], srcCanvasData.data[idxS10 + 3], srcCanvasData.data[idxS01 + 3], srcCanvasData.data[idxS11 + 3], dx, dy);
+            destCanvasData.data[idxD + 3] = a;
         }
     }
-
-    return bilinear;
-}());
-//
-//var loadCan = document.getElementById("load-canvas");
-//var dispCan = document.getElementById("disp-canvas");
-//
-//var loadCtx = loadCan.getContext("2d");
-//var dispCtx = dispCan.getContext("2d");
-//
-//var scale = 1.414;
-//
-//var image_var = new Image();
-//image_var.onload = function() {
-//    loadCan.setAttribute("width", image_var.width);
-//    loadCan.setAttribute("height", image_var.height);
-//    loadCan.style.position = "fixed";
-//    loadCan.width = image_var.width;
-//    loadCan.height = image_var.height;
-//    loadCtx.drawImage(image_var, 0, 0, image_var.width, image_var.height);
-//
-//    // getImageData : Chrome & FF: Unable to get image data from canvas because the canvas
-//    // has been tainted by cross-origin data.
-//    // when served from localhost, dev laptop
-//    var srcImg = loadCtx.getImageData(0, 0, image_var.width, image_var.height);
-//
-//    var newWidth = Math.ceil(image_var.width * scale);
-//    var newHeight = Math.ceil(image_var.height * scale);
-//    dispCan.width = newWidth;
-//    dispCan.height = newHeight;
-//    dispCan.setAttribute("width", newWidth);
-//    dispCan.setAttribute("height", newHeight);
-//    var destImg = dispCtx.createImageData(newWidth, newHeight);
-//    bilinear(srcImg, destImg, scale);
-//
-//    dispCtx.putImageData(destImg, 0, 0);
-//}
-//image_var.src = "http://www.philou.ch/pic/fear_makes_the_wolf_look_bigger.jpg";
+};
