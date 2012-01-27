67788f71f18dd3a081b50ee994e28d57f1cf0418
ross-tu <ross.turner@tradermedia.co.uk>
2012-01-27 11:44:16 +0000

    Incresed spike to use bilinear interpolation algorithm to 1024

--- src/main/webapp/js/ImageUploader.js
+++ src/main/webapp/js/ImageUploader.js
@@ -46,17 +46,20 @@ var ImageUploader = (function() {
                 canvas.height = img.height / 2;
 
                 config.workspace.appendChild(canvas);
-                
+
                 //thumbnailer(canvas, img, 1024, 1);
-                
+
+                // TODO draw straight from canvas to canvas with context.drawImage first
+                // argument as a canvas (should work!)
+
                 var context = canvas.getContext('2d');
                 context.drawImage(img, 0, 0, canvas.width, canvas.height);
-                
+
                 var resizedImage = document.createElement('img');
                 config.workspace.appendChild(resizedImage);
-                
+
                 resizedImage.src = canvas.toDataURL('image/jpeg');
-                
+
                 // ugh
                 setTimeout(function() {
                     var canvas2 = document.createElement('canvas');
@@ -66,15 +69,39 @@ var ImageUploader = (function() {
 
                     config.workspace.appendChild(canvas2);
                     canvas2.getContext('2d').drawImage(resizedImage, 0, 0, canvas2.width, canvas2.height);
-                    
+
                     var quarterImage = document.createElement('img');
                     config.workspace.appendChild(quarterImage);
-                    
+
                     quarterImage.src = canvas2.toDataURL('image/jpeg');
-                    
+
+                    setTimeout(function() {
+                        var canvas3 = document.createElement('canvas');
+
+                        var scale = 1024 / canvas2.width;
+
+                        canvas3.width = canvas2.width * scale;
+                        canvas3.height = canvas2.height * scale;
+                        config.workspace.appendChild(canvas3);
+
+                        var srcImg = canvas2.getContext('2d').getImageData(0, 0, canvas2.width, canvas2.height);
+                        var destImg = canvas3.getContext('2d').createImageData(canvas3.width, canvas3.height);
+
+                        BilinearInterpolation(srcImg, destImg, scale);
+
+                        canvas3.getContext('2d').putImageData(destImg, 0, 0);
+
+                        //canvas3.getContext('2d').drawImage(quarterImage, 0, 0, canvas3.width, canvas3.height);
+
+                        var sizedImage = document.createElement('img');
+                        config.workspace.appendChild(sizedImage);
+
+                        sizedImage.src = canvas3.toDataURL('image/jpeg');
+
+                    }, 300);
+
                 }, 300);
-                
-                
+
             }, 400);
         }
         reader.readAsDataURL(file);
