ddbdb1bed6d136d14878b9457ca92bf46f20d4f7
ross-tu <ross.turner@tradermedia.co.uk>
2012-01-27 14:46:59 +0000

    Removed some of the dead code

--- src/main/webapp/js/ImageUploader.js
+++ src/main/webapp/js/ImageUploader.js
@@ -92,47 +92,6 @@ var ImageUploader = (function() {
             console.log('Finished at ' + new Date().getTime());
             completionCallback();
         }
-        // ugh
-        //        setTimeout(function() {
-        //            var canvas2 = document.createElement('canvas');
-        //
-        //            canvas2.width = resizedImage.width / 2;
-        //            canvas2.height = resizedImage.height / 2;
-        //
-        //            config.workspace.appendChild(canvas2);
-        //            canvas2.getContext('2d').drawImage(resizedImage, 0, 0, canvas2.width, canvas2.height);
-        //
-        //            var quarterImage = document.createElement('img');
-        //            config.workspace.appendChild(quarterImage);
-        //
-        //            quarterImage.src = canvas2.toDataURL('image/jpeg');
-        //
-        //            setTimeout(function() {
-        //                var canvas3 = document.createElement('canvas');
-        //
-        //                var scale = 1024 / canvas2.width;
-        //
-        //                canvas3.width = canvas2.width * scale;
-        //                canvas3.height = canvas2.height * scale;
-        //                config.workspace.appendChild(canvas3);
-        //
-        //                var srcImgData = canvas2.getContext('2d').getImageData(0, 0, canvas2.width, canvas2.height);
-        //                var destImgData = canvas3.getContext('2d').createImageData(canvas3.width, canvas3.height);
-        //
-        //                BilinearInterpolation(srcImgData, destImgData, scale);
-        //
-        //                canvas3.getContext('2d').putImageData(destImgData, 0, 0);
-        //
-        //                //canvas3.getContext('2d').drawImage(quarterImage, 0, 0, canvas3.width, canvas3.height);
-        //
-        //                var sizedImage = document.createElement('img');
-        //                config.workspace.appendChild(sizedImage);
-        //
-        //                sizedImage.src = canvas3.toDataURL('image/jpeg');
-        //
-        //            }, 300);
-        //
-        //        }, 300);
 
     };
 
