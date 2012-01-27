e6c68baec8d5ee05db79db690512c396f816f463
ross-tu <ross.turner@tradermedia.co.uk>
2012-01-27 17:26:16 +0000

    Added JsLint back into the build

--- src/main/webapp/js/ImageUploader.js
+++ src/main/webapp/js/ImageUploader.js
@@ -1,8 +1,7 @@
-var console = console || {
-    log : function() {
-    }
-};
-
+/**
+ * ImageUploader.js - a client-side image resize and upload javascript module
+ * @author Ross Turner (https://github.com/zsinj)
+ */
 var ImageUploader = (function() {
     var publicApi = {};
     var privateApi = {};
@@ -14,12 +13,13 @@ var ImageUploader = (function() {
         if ((!customConfig.inputElement) || (!customConfig.inputElement.getAttribute) || customConfig.inputElement.getAttribute('type') !== 'file') {
             throw new Error('Config object passed to ImageUploader.init() must include "inputElement" set to be an element of type="file"');
         }
-
+        
         privateApi.setConfig(customConfig);
 
         config.inputElement.addEventListener('change', function(event) {
-            var fileArray = new Array();
-            for ( var cursor = 0; cursor < config.inputElement.files.length; ++cursor) {
+            var fileArray = [];
+            var cursor = 0;
+            for (; cursor < config.inputElement.files.length; ++cursor) {
                 fileArray.push(config.inputElement.files[cursor]);
             }
             privateApi.handleFileList(fileArray);
@@ -59,7 +59,7 @@ var ImageUploader = (function() {
                 privateApi.scaleImage(img, completionCallback);
             }, 1);
 
-        }
+        };
         reader.readAsDataURL(file);
 
     };
@@ -107,7 +107,7 @@ var ImageUploader = (function() {
         var srcImgData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
         var destImgData = scaledCanvas.getContext('2d').createImageData(scaledCanvas.width, scaledCanvas.height);
 
-        BilinearInterpolation(srcImgData, destImgData, scale);
+        privateApi.applyBilinearInterpolation(srcImgData, destImgData, scale);
 
         scaledCanvas.getContext('2d').putImageData(destImgData, 0, 0);
 
@@ -152,6 +152,56 @@ var ImageUploader = (function() {
 
         config.workspace = document.createElement('div');
         document.body.appendChild(config.workspace);
+    };
+    
+    // Modified from http://www.philou.ch/js-bilinear-interpolation.html
+    // Credit to Philippe Strauss
+    privateApi.applyBilinearInterpolation = function(srcCanvasData, destCanvasData, scale) {
+        // c.f.: wikipedia english article on bilinear interpolation
+        // taking the unit square, the inner loop looks like this
+        function inner(f00, f10, f01, f11, x, y) {
+            var un_x = 1.0 - x;
+            var un_y = 1.0 - y;
+            return (f00 * un_x * un_y + f10 * x * un_y + f01 * un_x * y + f11 * x * y);
+        }
+        var i, j;
+        var iyv, iy0, iy1, ixv, ix0, ix1;
+        var idxD, idxS00, idxS10, idxS01, idxS11;
+        var dx, dy;
+        var r, g, b, a;
+        for (i = 0; i < destCanvasData.height; ++i) {
+            iyv = i / scale;
+            iy0 = Math.floor(iyv);
+            // Math.ceil can go over bounds
+            iy1 = (Math.ceil(iyv) > (srcCanvasData.height - 1) ? (srcCanvasData.height - 1) : Math.ceil(iyv));
+            for (j = 0; j < destCanvasData.width; ++j) {
+                ixv = j / scale;
+                ix0 = Math.floor(ixv);
+                // Math.ceil can go over bounds
+                ix1 = (Math.ceil(ixv) > (srcCanvasData.width - 1) ? (srcCanvasData.width - 1) : Math.ceil(ixv));
+                idxD = (j + destCanvasData.width * i) * 4;
+                // matrix to vector indices
+                idxS00 = (ix0 + srcCanvasData.width * iy0) * 4;
+                idxS10 = (ix1 + srcCanvasData.width * iy0) * 4;
+                idxS01 = (ix0 + srcCanvasData.width * iy1) * 4;
+                idxS11 = (ix1 + srcCanvasData.width * iy1) * 4;
+                // overall coordinates to unit square
+                dx = ixv - ix0;
+                dy = iyv - iy0;
+                // I let the r, g, b, a on purpose for debugging
+                r = inner(srcCanvasData.data[idxS00], srcCanvasData.data[idxS10], srcCanvasData.data[idxS01], srcCanvasData.data[idxS11], dx, dy);
+                destCanvasData.data[idxD] = r;
+
+                g = inner(srcCanvasData.data[idxS00 + 1], srcCanvasData.data[idxS10 + 1], srcCanvasData.data[idxS01 + 1], srcCanvasData.data[idxS11 + 1], dx, dy);
+                destCanvasData.data[idxD + 1] = g;
+
+                b = inner(srcCanvasData.data[idxS00 + 2], srcCanvasData.data[idxS10 + 2], srcCanvasData.data[idxS01 + 2], srcCanvasData.data[idxS11 + 2], dx, dy);
+                destCanvasData.data[idxD + 2] = b;
+
+                a = inner(srcCanvasData.data[idxS00 + 3], srcCanvasData.data[idxS10 + 3], srcCanvasData.data[idxS01 + 3], srcCanvasData.data[idxS11 + 3], dx, dy);
+                destCanvasData.data[idxD + 3] = a;
+            }
+        }
     };
 
     return publicApi;
